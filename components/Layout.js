61f22381bce3679c2b31d111d1915ba752eaa6b5
LaggySon <56988314+LaggySon@users.noreply.github.com>
2022-04-06 19:44:06 -0400

    hotfix

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ function Layout(props) {
         <title>Tranquility Gaming</title>
         <style>
           @import
-          url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap');
+          url("https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap");
         </style>
       </Head>
       <div className="layout">
