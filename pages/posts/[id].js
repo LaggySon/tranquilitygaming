431621c22a75fdc29a866092d67a66ba341e1874
LaggySon <56988314+LaggySon@users.noreply.github.com>
2022-05-07 18:37:13 -0400

    added default route for /posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,34 +1,12 @@
-import { google } from "googleapis";
-
 export async function getServerSideProps({ query }) {
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-  });
-  const sheets = google.sheets({ version: "v4", auth });
-
-  const spreadsheetId = process.env.TRANCE_SHEET_ID;
-
-  var names = (
-    await sheets.spreadsheets.get({
-      spreadsheetId,
-    })
-  ).data.sheets.map((sheet) => {
-    return sheet.properties.title;
-  });
-  names = names.slice(1, names.length - 1);
+  const { id } = query;
   return {
     props: {
-      names,
+      id,
     },
   };
 }
 
-export default function Post({ names, poops }) {
-  return (
-    <div>
-      {names.map((name) => (
-        <div key={name}>{name}</div>
-      ))}
-    </div>
-  );
+export default function placeholder({ id }) {
+  return <div>{id}</div>;
 }
