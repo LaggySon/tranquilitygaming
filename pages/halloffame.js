27e0e200b29bb03e08b37fa5180f6edf2fac65d2
LaggySon <56988314+LaggySon@users.noreply.github.com>
2022-06-07 10:36:54 -0400

    started changing halloffame page

--- pages/halloffame.js
+++ pages/halloffame.js
@@ -1,12 +1,11 @@
 import Separator from "../components/Separator";
 import styles from "../styles/HallOfFame.module.scss";
+import data from "../public/hof.json";
 
-function HofClass(props) {
+function HofInductees(props) {
   return (
-    <>
-      <Separator>
-        <span>{props.when}</span> Inductees
-      </Separator>
+    <div className={styles.HofInductees}>
+      <h3>Inductees</h3>
       <div className={styles.HofList}>
         {props.list.map((name) => (
           <div className={styles.HofMember} key={name}>
@@ -14,220 +13,39 @@ function HofClass(props) {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
 function ChampTeam(props) {
   return (
-    <>
-      <Separator>
-        <span>
-          {props.when} {props.tier}
-        </span>{" "}
-        Champions
-      </Separator>
+    <div className={styles.ChampTeam}>
+      <h3>Champions</h3>
       <div className={styles.ChampMembers}>
         {props.list.map((name) => (
-          <div className={styles.HofMember} key={name}>
-            {name}
-          </div>
+          <div key={name}>{name}</div>
         ))}
       </div>
-    </>
+    </div>
+  );
+}
+
+function Season(props) {
+  return (
+    <div className={styles.season}>
+      <Separator>{props.when}</Separator>
+      {/* display inductees if they exist for the season */}
+      {props.list.inductees && <HofInductees list={props.list.inductees} />}
+    </div>
   );
 }
 
 export default function halloffame() {
-  const inauguralInductees = ["Dunzo", "Nerdables", "Orsted", "Vellzen"];
-  const s4inductees = [
-    "Cavalier",
-    "KourRage",
-    "MisterYeti",
-    "NPC",
-    "Specs",
-    "Thuggington",
-  ];
-  const s6inductees = [
-    "Bethcon2",
-    "Booth",
-    "Door",
-    "Impurse",
-    "Rokit",
-    "Vela",
-    "Zoobi3",
-  ];
-  const s8inductees = [
-    "Smuggzz",
-    "Chimanzy",
-    "Ice",
-    "SneakyKittens",
-    "Crispy",
-    "Kapitol",
-    "CrypticForce",
-  ];
-  const s1 = {
-    winningteam: {
-      name: "Havoc",
-      members: [
-        "Nerdables",
-        "Dunzo",
-        "Shashzilla",
-        "Satirical",
-        "KourRage",
-        "Smithpants",
-        "Esulive",
-        "Crackerboi",
-        "CrimsonSaber",
-        "Dark Requiem",
-      ],
-    },
-    //TODO add teams, MVP? to each player
-    allstars: {
-      dps: [
-        "Orsted",
-        "Zoobi3",
-        "F8TE",
-        "Garbagio",
-        "Vellzen",
-        "KourRage",
-        "OhStories",
-        "Maxix",
-      ],
-      tank: [
-        "CrypticForce",
-        "Dunzo",
-        "Evriin",
-        "Sabrespuck",
-        "Mimsie",
-        "Player",
-      ],
-      support: ["Cavalier", "Rush", "Nerdables", "Arise", "IvyCat", "Timojo"],
-    },
-  };
-  const s2 = {
-    //TODO add teams, role, MVP? to each player
-    winningteam: {
-      name: "Otaku",
-      members: [
-        "YoJack87",
-        "Klopsy",
-        "Flufflepuffs",
-        "Hybrid",
-        "Smuggzz",
-        "Cyph",
-        "Panda",
-        "PixelRocket",
-        "Mr.Bubbles (Coach)",
-        "PotatoGoddess (Coach)",
-        "Gaemmmm (Coach)",
-      ],
-    },
-
-    allstars: {
-      dps: [
-        "Smuggzz",
-        "Maxix",
-        "Vellzen",
-        "Anarchy",
-        "PetersGG",
-        "GrumpyPants",
-        "Guac",
-      ],
-      tank: ["Kingdom", "SilentMonkey", "YoJack87", "NPC", "HQC", "Dunzo"],
-      support: [
-        "Arise",
-        "Laze",
-        "Flufflepuffs",
-        "Shadowph33r",
-        "IvyCat",
-        "MixReddy",
-      ],
-    },
-  };
-  const s3 = {
-    //TODO add teams, role, MVP? to each player
-    harmony: {
-      winningteam: {
-        name: "HOLY JALAPEÑOS",
-        members: [
-          "TheMallard",
-          "TorbOnly",
-          "BethCon2",
-          "JohnnyRobot",
-          "N33dul",
-          "THEBork",
-          "Meebles",
-          "Spooderman (Coach)",
-        ],
-      },
-
-      allstars: {
-        dps: ["Kurt", "00H", "PDKKid", "Gekk0", "Triage3", "Cremsin"],
-        tank: [
-          "NPC",
-          "Torraction",
-          "Impurse",
-          "Crispy",
-          "Player",
-          "Bebop",
-          "Rigmedon",
-        ],
-        support: [
-          "Sushi",
-          "Crygenix",
-          "YourTeammate",
-          "N33Dul",
-          "DrugsNotHugs",
-          "Crossfade",
-        ],
-      },
-    },
-    discord: {
-      winningteam: {
-        name: "KOMMIT",
-        members: [
-          "Ice",
-          "SirAndy",
-          "Exas",
-          "Koro",
-          "Axiom",
-          "HoneyBadger",
-          "Shiro",
-          "Raidbotti",
-          "MyBigMarf",
-        ],
-      },
-
-      allstars: {
-        dps: [
-          "Rokit",
-          "Shiro",
-          "Smuggzz",
-          "JVice",
-          "Pyro",
-          "Kaizen",
-          "OverNightCDR",
-        ],
-        tank: [
-          "Kingdom",
-          "Sir Andy",
-          "CrypicForce",
-          "KourRage",
-          "Bandito",
-          "HQC",
-          "Booth",
-        ],
-        support: ["Ice", "Arise", "Koro", "Juke", "Unkempt", "Veets"],
-      },
-    },
-  };
+  console.log(data.s3);
   return (
     <div className={"container " + styles.HofContainer}>
-      <HofClass when="Inaugural" list={inauguralInductees} />
-      <HofClass when="Season 4" list={s4inductees} />
-      <HofClass when="Season 6" list={s6inductees} />
-      <HofClass when="Season 8" list={s8inductees} />
+      <Season when="Inaugural Season" list={data.s1}></Season>
+      <Season when="Season 2" list={data.s2}></Season>
     </div>
   );
 }
