b624b355737b7102c89f16aff75fa86fa8cd897c
LaggySon <56988314+LaggySon@users.noreply.github.com>
2022-10-02 13:09:52 -0400

    added CMS integration for broadcast schedule

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,9 @@ import { StructuredText } from "react-datocms";
 import { request } from "../lib/datocms";
 
 export async function getServerSideProps() {
-  const data = await request({ query: `query {homepage{letter{value}}}` });
+  const data = await request({
+    query: `query {homepage{letter{value},schedule{value}}}`,
+  });
   console.log(data);
   return { props: { data } };
 }
@@ -76,55 +78,17 @@ export default function Home({ data }) {
         </div>
 
         <Separator>
+          <span>UPCOMMING</span> BROADCASTS
+        </Separator>
+        <div id={styles.letter} className="blockel">
+          <StructuredText data={data.homepage.schedule} />
+        </div>
+        <Separator>
           <span>LATEST</span> NEWS
         </Separator>
 
         <div id={styles.letter} className="blockel">
           <StructuredText data={data.homepage.letter} />
-          {/* <h2>Letter From The Commissioner</h2>
-          <p>
-            Welcome to the Tranquility Gaming Community! This community is
-            intended to aid in the growth of players, coaches, managers and
-            associated contributors; furthering their development in online
-            esports with a particular focus on Overwatch. Tranquility was first
-            conceived as a casual intramural level tournament but has since
-            evolved into a developmental community helping players across
-            multiple games and skill tiers. Our Overwatch tournament currently
-            runs over a 17 week season style format bi-annually.
-            <br />
-            <br />
-            Tranquility is intended to be a well-organized community and will
-            take structure, branding, production and operation seriously. It’s a
-            community that is meant to be fun and drama free, but that only
-            works if everyone participates and follows the developed guidelines.
-            <br />
-            <br />
-            After eight successful seasons we are gearing up to kick off season
-            9 this coming autumn 2022 with an expanded group that now contains
-            five total tiers.
-            <br />
-            <br />
-            NA & EU - Harmony Tier (2000-3000 w/ 2650 SR avg) <br />
-            NA & EU - Discord Tier (2800-3500 w/ 3200 SR avg) <br />
-            NA - Transcendence Tier (3300-4000 w/ 3750* SR avg)
-            <br />
-            <br />
-            *Top six players only count towards team average.
-            <br />
-            <br />
-            Tranquility Gaming is open to all players looking to help grow and
-            expand esports. It is meant to be an inclusive space to help build a
-            community of production, competition, and friendship.
-            <br />
-            <br />I look forward to meeting and interacting with you all, and I
-            hope we can grow Tranquility into the best it can be!
-            <br />
-            <br />
-            Regards, <br />
-            Cavalier <br />
-            Founder & Commissioner <br />
-            Tranquility Gaming
-          </p> */}
         </div>
       </div>
     </div>
